ce0ecef5618d9cc9ed4602d5d4b6c7e8bf0e176a
apple <usamamahaar@gmail.com>
2024-07-22 15:26:35 +0500

    with docker swarm

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'supersecretkey'
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://root:example@mariadb/flask_db'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:example@mariadb/flaskdb'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:example@db/flaskdb'
